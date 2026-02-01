e2da0b0fc9f4856c731246e4b4ada75dd2069b7f
MrFasolo97 <79fasolo97@gmail.com>
2026-02-01 23:24:53 +0100

    chore(version): bump version data

--- client/lib/version.js
+++ client/lib/version.js
@@ -1,1 +1,1 @@
-export const DTubeVersion = {commit: '6798b535cbfe8bc20952e12ea03409a8e47f6569', branch: 'meteor3', timestamp: '2026-01-20 18:44'};
+export const DTubeVersion = {commit: 'bbc338fb3930568d17d6e9be4aaba167aae71566', branch: 'meteor3', timestamp: '2026-02-01 22:23'};
